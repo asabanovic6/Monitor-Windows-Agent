27521299fed0972a1e6b7f928d10a024a5b13391
Amina Sabanovic <asabanovic6@etf.unsa.ba>
2021-03-16 22:42:43 +0100

    Parsiran json file

---
+++ JASONParser/parser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace JASONParser
+{
+    public class parser
+    {
+        
+           
+      
+        public class ComputerInfo
+        {
+            public String Computer_name { get; set; }
+            public decimal Keep_alive { get; set; }
+        }
+
+        public  ComputerInfo ConfigParser()
+        {
+            var json = File.ReadAllText("../../../../config.json");
+
+            var item = JsonConvert.DeserializeObject<ComputerInfo>(json);
+
+            return item;
+        }
+
+    }
+}
--- Monitor-Windows-Agent/Program.cs
+++ Monitor-Windows-Agent/Program.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+
 
 namespace Monitor_Windows_Agent
 {
@@ -11,9 +14,13 @@ namespace Monitor_Windows_Agent
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
+        /// 
+
+        
         [STAThread]
         static void Main()
         {
+         
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
