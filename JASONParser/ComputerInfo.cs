aade4e1817bbf36e85a96992aa526bf911fa7bd7
Amina Sabanovic <asabanovic6@etf.unsa.ba>
2021-03-19 16:05:52 +0100

    'novipokusaj'

---
+++ JASONParser/ComputerInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace JASONParser
+{
+    public class ComputerInfo
+    {
+        public String name { get; set; }
+        public decimal keepAlive { get; set; }
+        public String location { get; set; }
+    }
+}
---
+++ Monitor-Windows-Agent/CreateJSON.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using JASONParser;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Monitor_Windows_Agent
+{
+    class CreateJSON
+    {
+        
+        private static Parser parser = new Parser();
+        private static ComputerInfo computer = parser.ConfigParser();
+     public void getJson ()
+        {
+           //ovdje kreiramo objekat 
+            
+        }
+    }
+    }
+
