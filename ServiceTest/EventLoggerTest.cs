8f626390a325cca54cdd72476fad8c7770b29aca
ekarahodza1 <ekarahodza1@etf.unsa.ba>
2021-05-02 01:17:31 +0200

    kreiran testni projekat

---
+++ ServiceTest/EventLoggerTest.cs
@@ -0,0 +1,19 @@
+﻿using EventLogger;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ServiceTest
+{
+    [TestClass]
+    public class EventLoggerTest
+    {
+        [TestMethod]
+        public void CreateMessageTest()
+        {
+            Logger l = new Logger("", "");
+            //l.createLog();
+            l.createMessage();
+            l.deleteLog();
+        }
+    }
+}
---
+++ ServiceTest/JASONParserTest.cs
@@ -0,0 +1,32 @@
+﻿using JASONParser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Monitor_Windows_Agent;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceTest
+{
+    [TestClass]
+    public class JASONParserTest
+
+    {
+        [TestMethod, ExpectedException(typeof(NullReferenceException))]
+        public void ReadingDirectoryTest()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\config.json";
+            Parser p = new Parser();
+            Assert.AreEqual(path, p.ConfigParser().fileUri);
+        }
+
+        [TestMethod, ExpectedException(typeof(NullReferenceException))]
+        public void CreateJSONTest()
+        {
+            //CreateJSON info = new CreateJSON();
+            Assert.AreNotEqual("", CreateJSON.getJSON());
+        }
+    }
+}
