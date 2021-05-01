2a0509beda2b3228804d68a2d6d3836d2eb85bfb
asuljagic <43003728+asuljagic@users.noreply.github.com>
2021-05-01 18:53:01 +0200

    HDDUsage test case

--- UnitTestProject/TerminalCommandTest.cs
+++ UnitTestProject/TerminalCommandTest.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Terminal;
+
 
 namespace UnitTestProject
 {
-    class TerminalCommandTest
+    [TestClass]
+   public class TerminalCommandTest
     {
+        [TestMethod]
+        public void HDDUsageTest()
+        {
+            double usage = TerminalCommand.getHDDUsage();
+            Assert.IsTrue(0 <= usage && usage <= 1);
+        }
+
+       
+
+        
     }
 }
